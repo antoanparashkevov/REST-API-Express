6341b4451ce4aaed3d2c35de935fbef92941da9a
“antoanparashkevov” <tonkata1505@gmail.com>
2022-11-03 18:59:09 +0200

    Regiser service DONE

---
+++ server/services/authService.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+const SECRET_KEY = 'MYSECRETKEY'
+async function register(email,password) {
+    const isExisting = await User.find({email}).collation({ locale:'en', strength:2 })
+    
+    if( isExisting ) {
+        throw new Error('Email is taken')
+    }
+    const hashedPassword = bcrypt.hash(password,10)
+    
+    const user = await User.create({
+        email,
+        hashedPassword
+    });
+    return createToken(user)//return the token when we await register function
+    
+}
+async function login(email,password) {}
+async function logout(email,password) {}
+
+const createToken = function(user) {
+    const payload = {
+        _id: user._id,
+        email: user.email
+    };
+    
+   return jwt.sign(payload, SECRET_KEY)//returns the token;
+    
+}

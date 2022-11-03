b14a395b2a533d283144c129aa5dd6be17911deb
“antoanparashkevov” <tonkata1505@gmail.com>
2022-11-03 18:50:00 +0200

    User schema

--- server/server.js
+++ server/server.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const app = express();
 const cors = require('./middlewares/cors')
+const mongoose = require("mongoose");
 const port = 3030;
+const CONNECTION_STR = 'mongodb://localhost:27017/furniture'
 
-app.use(express.json())
-app.use(cors())
+start();
+
+async function start() {
+
+    try {
+        mongoose.connect(CONNECTION_STR, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        })
+        console.log('DATABASE connected!')
+    } catch (err) {
+        console.error(err.message)
+    }
+
+    app.use(express.json())
+    app.use(cors())
+}
 
 
 app.listen(port, ()=>console.log('Server listening on port ' + port))

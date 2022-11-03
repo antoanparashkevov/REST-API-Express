4d432dc19f0fb1169c61ea3b3c93b9097de61da6
“antoanparashkevov” <tonkata1505@gmail.com>
2022-11-03 18:42:01 +0200

    Middleware for CORS

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express();
+const cors = require('./middlewares/cors')
 const port = 3030;
 
-app.use(express.static('static'))
 app.use(express.json())
+app.use(cors())
 
 
 app.listen(port, ()=>console.log('Server listening on port ' + port))
